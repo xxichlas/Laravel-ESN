0cdabe64cdb7fc71c7016d6a202c41cee18db679
xxichlas <72259352+xxichlas@users.noreply.github.com>
2021-01-10 19:05:21 +0800

    minor changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
                 <div class="col-md-12">
                     <!-- Advanced Tables -->
-                   <H1>Selamat datang {{ Auth::user()->name }} di dashboard ESN!</H1>
+                   <H1>Selamat datang {{ Auth::user()->name }} didashboard ESN!</H1>
                         </div>
                         </div>
                       
